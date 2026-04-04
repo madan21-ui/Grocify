c346502fceac4f91744073087c451dce7fc89105
madan21-ui <mehunmadan@gmail.com>
2026-04-04 21:22:58 +0500

    feat: add sign-in screen and update configuration for Clerk integration

--- my-app/babel.config.js
+++ my-app/babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "@react-native/babel-preset",
       "nativewind/babel",
     ],
   };
