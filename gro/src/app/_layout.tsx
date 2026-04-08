f44c26a5b7f49a459ae819c5c20ef98df1062e44
madan21-ui <mehunmadan@gmail.com>
2026-04-08 11:56:21 +0500

    erased

--- gro/src/app/_layout.tsx
+++ gro/src/app/_layout.tsx
@@ -1,21 +1,16 @@
-import { Stack } from "expo-router";
-import "../../global.css";
-import { ClerkProvider } from '@clerk/expo'
-import { tokenCache } from '@clerk/expo/token-cache'
-import { Slot } from 'expo-router'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import React from 'react';
+import { useColorScheme } from 'react-native';
 
+import { AnimatedSplashOverlay } from '@/components/animated-icon';
+import AppTabs from '@/components/app-tabs';
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
-
-if (!publishableKey) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
-}
-
-export default function RootLayout() {
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
   return (
-  <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Stack screenOptions={{headerShown: false}}/>
-    </ClerkProvider>
-
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <AnimatedSplashOverlay />
+      <AppTabs />
+    </ThemeProvider>
   );
 }
