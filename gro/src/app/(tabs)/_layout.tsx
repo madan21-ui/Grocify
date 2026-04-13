68ff934cfcb50d9fd029ecee30f133f3d9bfe1ae
madan21-ui <mehunmadan@gmail.com>
2026-04-13 22:59:10 +0500

    adding navigation

--- gro/src/app/(tabs)/_layout.tsx
+++ gro/src/app/(tabs)/_layout.tsx
@@ -1,7 +1,8 @@
 import { useAuth } from '@clerk/expo'
 import { Redirect, Stack } from 'expo-router'
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
 
-export default function Layout() {
+export default function TabsLayout() {
   const { isSignedIn, isLoaded } = useAuth()
 
   if (!isLoaded) {
@@ -12,5 +13,15 @@ export default function Layout() {
     return <Redirect href="/(auth)/sign-in" />
   }
 
-  return <Stack />
+  return <NativeTabs>
+    <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="planner">
+        <NativeTabs.Trigger.Icon sf="plus.circle" md="add" /> //md for android and sf for ios
+        <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+  </NativeTabs>
 }
