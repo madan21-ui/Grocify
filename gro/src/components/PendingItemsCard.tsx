cd0557779772236c71cb7adcf74589bd73dcc99b
madan21-ui <mehunmadan@gmail.com>
2026-04-16 23:01:05 +0500

    Refactor PendingItemsCard to include priority styles and enhance layout

--- gro/src/components/PendingItemsCard.tsx
+++ gro/src/components/PendingItemsCard.tsx
@@ -2,11 +2,27 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { GroceryItem, useGroceryStore } from '@/store/grocery-store'
 
+
+const priorityPillBg = {
+  low: "bg-priority-low",
+  medium: "bg-priority-medium",
+  high: "bg-priority-high",
+};
+
+const priorityPillText = {
+  low: "text-priority-low-foreground",
+  medium: "text-priority-medium-foreground",
+  high: "text-priority-high-foreground",
+};
+
+
+
 const PendingItemsCard = ({item}:{item:GroceryItem}) => {
     const {removeItem,updateQuantity, togglepurchased} = useGroceryStore();
+
+    
   return (
-    <View>
-      <Text>PendingItemsCard</Text>
+    <View className="rounded-3xl border border-border bg-card p-4">
     </View>
   )
 }
