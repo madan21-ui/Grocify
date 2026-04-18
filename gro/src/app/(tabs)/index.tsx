bd3b549ce6eb901f2dd5b86bedeafcf562dc2a92
madan21-ui <mehunmadan@gmail.com>
2026-04-18 20:24:16 +0500

    pendingItemmap is replaced with

--- gro/src/app/(tabs)/index.tsx
+++ gro/src/app/(tabs)/index.tsx
@@ -1,9 +1,49 @@
 import { TabScreenBackground } from '@/components/TabScreenBackground';
 import { useGroceryStore } from '@/store/grocery-store';
-import { Text, View,  ScrollView } from 'react-native'
+import { Text, View,  ScrollView, FlatList } from 'react-native'
 import ListHeroCard from '@/components/list/ListHeroCard';
 import PendingItemCard from '@/components/PendingItemsCard';
 import CompletedItems from '@/components/list/CompletedItems ';
+
+export default function ListScreen() {
+
+  const { items } = useGroceryStore();
+  const pendingItems = items.filter((item) => !item.purchased);
+  
+
+
+
+  return (
+    < ScrollView className='flex-1 bg-background py-4 '
+    showsVerticalScrollIndicator={false}
+    contentContainerStyle={{ padding: 20 , gap: 14}}
+    >
+     <TabScreenBackground />
+     <ListHeroCard/>
+
+     <View className="flex-row items-center justify-between px-1">
+        <Text className="text-sm font-semibold uppercase tracking-[1px] text-muted-foreground">
+          Shopping items
+        </Text>
+        <Text className="text-sm text-muted-foreground">{pendingItems.length} active</Text>
+      </View>
+
+      <FlatList
+      data={pendingItems}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PendingItemCard item={item} />}
+      />
+     
+
+      <CompletedItems/>
+    </ScrollView>
+  );
+}
+
+
+//Previous version with pendingItem.map
+
+/*
 
 export default function ListScreen() {
 
@@ -35,3 +75,5 @@ export default function ListScreen() {
     </ScrollView>
   );
 }
+
+*/
