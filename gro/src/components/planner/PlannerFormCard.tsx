f96bb38eb646fbf3e9180877c5e1cf280c7394c6
madan21-ui <mehunmadan@gmail.com>
2026-04-19 10:59:58 +0500

    Refactor PlannerFormCard to include useGroceryStore hook and reorganize state management

--- gro/src/components/planner/PlannerFormCard.tsx
+++ gro/src/components/planner/PlannerFormCard.tsx
@@ -1,20 +1,38 @@
 import { View, Text } from 'react-native'
 import React, { useState } from 'react'
-import { GroceryCategory, GroceryPriority } from '@/store/grocery-store';
+import { GroceryCategory, GroceryPriority, useGroceryStore } from '@/store/grocery-store';
 
 const categories:GroceryCategory[] = ["produce", "dairy", "snacks", "bakery", "pantry", "meat", "beverages", "household"];
 const priorities:GroceryPriority[] = ["low","medium","high"];
-export default function PlannerFormCard() {
 
-    const [ name, setName ] = useState("");
-    const [ quantity, setQuantity ] = useState("1");
-    const categoryIcon ={
+const categoryIcon ={
         produce: "leaf",
         dairy: "cow",
         snacks: "cookie-bite",
         bakery: "bread-slice",
         pantry: "boxx-open"
-    }
+    };
+export default function PlannerFormCard() {
+    const { error, addItem } = useGroceryStore();
+
+    const [ name, setName ] = useState("");
+    const [ quantity, setQuantity ] = useState("1");
+    const [ category, setCategory ] = useState<GroceryCategory>("produce");
+    const [ priority, setPriority ] = useState<GroceryPriority>("medium");
+
+    const canCreate = name.trim().length > 0 ;
+    const handleQuantityChang = (value:string) =>{
+        // this regex is used to remove all non-numeric characters from the input
+        setQuantity(value.replace(/[^0-9]/g, ""));
+    };
+
+    const createItem = async () => {
+    await addItem({
+      name: name.trim(),
+      category,
+      priority,
+      quantity: Number(quantity),
+    });
 
 
 
