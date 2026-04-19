e768a2da6d650e04cf08a36812a344976fa2ac85
madan21-ui <mehunmadan@gmail.com>
2026-04-19 23:10:30 +0500

    Add UserProfile component for user information display and sign-out functionality

--- gro/src/app/(tabs)/insight.tsx
+++ gro/src/app/(tabs)/insight.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { TabScreenBackground } from '@/components/TabScreenBackground'
+import UserProfile from '@/components/insight/userProfile';
+import { useClerk, useUser } from '@clerk/expo';
 
 const insightScreen = () => {
   return (
-    <View>
-      <Text>insightScreen</Text>
-    </View>
-  )
-}
+    <ScrollView className='flex-1 bg-background py-4'
+    showsVerticalScrollIndicator={false}
+    contentContainerStyle={{ padding: 20 , gap: 14 }}
+    contentInsetAdjustmentBehavior='automatic'
+    >
+
+     <TabScreenBackground/>
+
+     <UserProfile />
+
+    </ScrollView>
+  );
+};
 
 export default insightScreen
